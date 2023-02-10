b5ec9a3b6d401ba85ee81d02bfc9d049ff2a105c
Ashiq Ummathoor <ashiqit93@gmail.com>
2023-02-10 17:11:43 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "ec2" {
   ami = "ami-0b0dcb5067f052a63"
-  instance_type = "t2.micro"
+  instance_type = "t2.medium"
   subnet_id = aws_subnet.subnet1.id
   user_data = file("shell.sh")
   vpc_security_group_ids = [aws_security_group.sgforinstance.id]
